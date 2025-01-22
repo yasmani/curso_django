f6078fb07ec4f66dfd818d98fafdc17baf93c093
Yasmani Gutierrez <32417638+yasmani@users.noreply.github.com>
2025-01-21 20:35:02 -0400

    laboratorio 3

--- ejercicio1/urls.py
+++ ejercicio1/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import hola_mundo
+#from .views import hola_mundo
+from .views import index
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    
+    path('',index, name='index')
 
-    path('', hola_mundo, name='ejercicio1'),
+    #path('', hola_mundo, name='ejercicio1'),
     
 ]
--- ejercicio1/views.py
+++ ejercicio1/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def hola_mundo(request):
-    return HttpResponse("Hola mundo..")
+#def hola_mundo(request):
+    #return HttpResponse("Hola mundo..")
+    
+def index(request):
+    return render(request,'index.html')
