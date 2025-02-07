17e641b0cd4a72d7689dcfd415efc90cb97a9ec9
Yasmani Gutierrez <32417638+yasmani@users.noreply.github.com>
2025-02-06 21:29:42 -0400

    laboratorio 7-8

--- laboratorio7/gestorcontactos/contacto/urls.py
+++ laboratorio7/gestorcontactos/contacto/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.sispa_index, name='sispa_index'),
     path('contacto/', views.contacto_lista, name='contacto_lista'),
     path('agregar/', views.contacto_agregar, name='contacto_agregar'),
+    path('eliminar/', views.elimina_contacto, name='elimina_contacto'),
+     path('editar/<int:pk>', views.contacto_editar, name='contacto_editar'),
 ]
--- laboratorio7/gestorcontactos/contacto/views.py
+++ laboratorio7/gestorcontactos/contacto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Contacto
 from .forms import FormularioContacto
 
@@ -24,3 +24,19 @@ def contacto_agregar(request):
     
     
     #realizar el CRUD
+
+def contacto_editar(request, pk):
+    contacto=get_object_or_404(Contacto, pk=pk)
+    if request.method == "POST":
+        formulario = FormularioContacto(request.POST, instance=contacto)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('contacto_lista')
+    else:
+        formulario=FormularioContacto(instance=contacto)
+    return render(request, 'contacto/contacto_formulario.html',{'formulario': formulario})
+    
+    
+def elimina_contacto(request):
+    return redirect('contacto_lista')
+     
